33c438591cfbf611d110b419bb25553e20e6a902
VanyaHuaman <vanyahuaman@gmail.com>
2023-12-04 12:04:43 -0600

    Remove duplicate license call

--- build_forge.py
+++ build_forge.py
@@ -6,13 +6,12 @@ def light_forge():
     helpers.check_for_sdk_manager()
     commands = helpers.build_command_list()
     tasks = helpers.build_task_list(commands)
-    helpers.accept_win_license()
     helpers.launch_tasks(tasks)
 
 
 if __name__ == '__main__':
     if not helpers.is_java_home_set():
-        print("ERROR: ANDROID_HOME ENV VAR IS NOT SET")
+        print("ERROR: JAVA_HOME ENV VAR IS NOT SET")
     elif not helpers.is_android_home_set():
         print("ERROR: ANDROID_HOME ENV VAR IS NOT SET")
     else:
